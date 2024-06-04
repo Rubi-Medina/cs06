e11ef1067d37465f608553030251e903cfb0c894
Alondra <arqramm@gmail.com>
2024-06-03 22:46:04 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Yuju he conectado"); /* poner mensajes puedes visualizar en consola de la pag de internet */
+function greeting(){
+    alert("y mas alla"); /* funcion de alerta*/
+}
+greeting(); /*  para invocar la funcion de saludo */
